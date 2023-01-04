ff447e4bfbd857c57cd3bdaa926b7668020bc09c
HassaanAhmed1 <Hassaan@visiomate.com>
2023-01-04 15:00:50 +0500

    Updates

---
+++ precache-manifest.fb780dedaa8fc9583c7294b2c5e52bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1c8e9be803442448eaa",
+    "url": "/monsters-rolodex/static/css/main.0b381e7d.chunk.css"
+  },
+  {
+    "revision": "e1c8e9be803442448eaa",
+    "url": "/monsters-rolodex/static/js/main.e1c8e9be.chunk.js"
+  },
+  {
+    "revision": "2e76ef4f4b07ab805959",
+    "url": "/monsters-rolodex/static/js/1.2e76ef4f.chunk.js"
+  },
+  {
+    "revision": "ebd5b217692bce4a6d85",
+    "url": "/monsters-rolodex/static/js/2.ebd5b217.chunk.js"
+  },
+  {
+    "revision": "9875e39d3066d0b779b7",
+    "url": "/monsters-rolodex/static/js/runtime~main.9875e39d.js"
+  },
+  {
+    "revision": "9497c433968db1f2e728e5b2ddc0443b",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
